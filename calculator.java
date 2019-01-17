bc1acf7f1ff5f9f94faee351a4301f18b978df71
Tripti Agarwal <tripti.agarwal@iiitb.org>
2019-01-17 12:10:49 +0530

    subtract 2

--- calculator.java
+++ calculator.java
@@ -8,8 +8,9 @@ public class calculator
 		Scanner scan = new Scanner(System.in);
 		do
 		{
-			System.out.println("1. Subtract 2 Numbers \n");
-			System.out.println("2. Exit \n\n");
+			System.out.println("1. Add 2 Numbers \n");
+			System.out.println("2. Subtract 2 Numbers \n");
+			System.out.println("3. Exit \n\n");
 			System.out.println("Enter Your Choice: ");
 			choice = scan.next().charAt(0);
 			switch(choice)
@@ -17,11 +18,17 @@ public class calculator
 				case '1' :	System.out.println("Enter 2 Numbers: ");
 							a = scan.nextFloat();
 							b = scan.nextFloat();
+							res = a + b;
+							System.out.println("Result = " + res);
+							break;
+				case '2' :	System.out.println("Enter 2 Numbers: ");
+							a = scan.nextFloat();
+							b = scan.nextFloat();
 							res = a - b;
 							System.out.println("Result = " + res);
 							break;
 
-				case '2' :	System.exit(0);
+				case '3' :	System.exit(0);
 							break;
 
 				default  :	System.out.println("Invalid Choice!!!");
@@ -29,6 +36,6 @@ public class calculator
 			}
 			System.out.println("\n ------------------------- \n");
 
-		}while(choice != 2);
+		}while(choice != 3);
 	}
 }
